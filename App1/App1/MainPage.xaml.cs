ce78594b354019c7a28f0a3ca3a5e51f3bae5868
JulioE3274 <pineapplelab13@gmail.com>
2020-02-13 17:00:30 -0300

    Add project files.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.IO;
+
+namespace App1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+        int count = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+
+        private void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+
+        private void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
+        }
+    }
+}
